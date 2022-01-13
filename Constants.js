19b5a6875d93fceabed280c41539e7b00906a996
siiddharth <sharmasid2398@gmail.com>
2022-01-13 03:11:37 +0000

    first commit

---
+++ Constants.js
@@ -0,0 +1,5 @@
+/* For Best Practices https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables*/
+/*export const API_URL = 'http://localhost:8080'*/
+export const API_URL = 'https://example-app-service-hu19.azurewebsites.net'
+//export const API_URL = 'https://rest-api-full-stack-in28minutes.azurewebsites.net'
+export const JPA_API_URL = `${API_URL}/jpa`
